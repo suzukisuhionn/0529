400b360a051cb8d3a2a6b263af5c1c84931f47a6
suzukisuhionn <130746580+suzukisuhionn@users.noreply.github.com>
2023-04-14 18:32:11 +0900

    a

---
+++ dist/assets/fadein-for-student/for-loop-python/sample.py
@@ -0,0 +1,13 @@
+# 変数の勉強
+maguro = "マグロ"
+salmon = "サーモン"
+
+print(maguro)
+
+# 配列を作ってデータを丸ごと入れることもできる
+sushi = ["マグロ", "サーモン", "イカ", "アナゴ", "イクラ"]
+
+sushi_num = len(sushi)
+
+for i in range(sushi_num):
+    print(sushi[i])
---
+++ dist/css/fadein-for-student/for-loop-python/sample.py
@@ -0,0 +1,18 @@
+# 変数の勉強
+maguro = "マグロ"
+salmon = "サーモン"
+
+#print(maguro)
+
+# 配列を作ってデータを丸ごと入れることもできる
+sushi = ["マグロ", "サーモン", "イカ", "アナゴ", "イクラ","たまご"]
+
+sushi_num = len(sushi)
+
+#print(sushi_num)
+
+#print(sushi[-5])
+
+for i in range(sushi_num):
+   print(i)
+   
